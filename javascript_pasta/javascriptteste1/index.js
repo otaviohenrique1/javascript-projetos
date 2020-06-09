f1dca515da1e5a731b264de1e512194611996b55
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2020-06-09 12:35:58 -0300

    Add files via upload

---
+++ javascript_pasta/javascriptteste1/index.js
@@ -0,0 +1,6 @@
+// a-b
+// c-x
+function calculaMedida(a,b,c) {
+    return (parseFloat(a)*parseFloat(b))/parseFloat(c);
+}
+console.log(`${calculaMedida(370, 130, 300)}`);
---
+++ javascript_teste/temperatura.js
@@ -0,0 +1,9 @@
+export const converteCelsiusEmKelvin = (C) => {
+    let K = C + 273.15;
+    return K;
+}
+
+export const converteKelvinEmCelsius = (K) => {
+    let C = K - 273.15
+    return C;
+}
